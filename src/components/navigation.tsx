21365e256a365689bf2d7eff92343df95820f0a6
Makenna Smutz <makennasmutz@gmail.com>
2021-03-14 16:26:43 +0100

    case change

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,72 +1,71 @@
 import React, { useContext } from "react";
 import {
-  Flex,
-  Link as ChakraLink,
-  Image,
-  Stack,
-  Box,
-  Select,
+	Flex,
+	Link as ChakraLink,
+	Image,
+	Stack,
+	Box,
+	Select,
 } from "@chakra-ui/react";
 import LocaleContext, { locales } from "../components/context/get-locale";
 
 const Navigation = () => {
-  const { currentLocale, updateLocale } = useContext(LocaleContext);
+	const { currentLocale, updateLocale } = useContext(LocaleContext);
 
-  const emoji = (countryCode: string) =>
-    countryCode
-      .toUpperCase()
-      .replace(/./g, (char) =>
-        String.fromCodePoint(char.charCodeAt(0) + 127397)
-      );
+	const emoji = (countryCode: string) =>
+		countryCode
+			.toUpperCase()
+			.replace(/./g, (char) =>
+				String.fromCodePoint(char.charCodeAt(0) + 127397)
+			);
 
-  console.log(emoji);
-  return (
-    <Flex
-      position="fixed"
-      direction="column"
-      justify="space-between"
-      align="center"
-      left={0}
-      top={0}
-      bottom={0}
-      py={4}
-      backgroundColor="white"
-      boxShadow="0px 8px 24px 0px rgba(149,157,165,0.2)"
-    >
-      <Image h={48} src={`https://media.graphcms.com/3LKjkQiSBWIVZMvJ31pj`} />
-      <Stack direction="column">
-        <ChakraLink href="/" px={2}>
-          About us
-        </ChakraLink>
-        <Box className="relative">
-          <Select
-            size="sm"
-            value={currentLocale}
-            borderRadius="md"
-            onChange={({ target: { value } }) => updateLocale(value)}
-          >
-            {locales.map(({ label }, index) => (
-              <option key={index} value={label}>
-                {`${emoji(
-                  label === "Slovenian"
-                    ? "SI"
-                    : label === "English"
-                    ? "US"
-                    : null
-                )} ${
-                  label === "Slovenian"
-                    ? "Slo"
-                    : label === "English"
-                    ? "En"
-                    : null
-                }`}
-              </option>
-            ))}
-          </Select>
-        </Box>
-      </Stack>
-    </Flex>
-  );
+	return (
+		<Flex
+			position="fixed"
+			direction="column"
+			justify="space-between"
+			align="center"
+			left={0}
+			top={0}
+			bottom={0}
+			py={4}
+			backgroundColor="white"
+			boxShadow="0px 8px 24px 0px rgba(149,157,165,0.2)"
+		>
+			<Image h={48} src={`https://media.graphcms.com/3LKjkQiSBWIVZMvJ31pj`} />
+			<Stack direction="column">
+				<ChakraLink href="/" px={2}>
+					About us
+				</ChakraLink>
+				<Box className="relative">
+					<Select
+						size="sm"
+						value={currentLocale}
+						borderRadius="md"
+						onChange={({ target: { value } }) => updateLocale(value)}
+					>
+						{locales.map(({ label }, index) => (
+							<option key={index} value={label}>
+								{`${emoji(
+									label === "Slovenian"
+										? "SI"
+										: label === "English"
+										? "US"
+										: null
+								)} ${
+									label === "Slovenian"
+										? "Slo"
+										: label === "English"
+										? "En"
+										: null
+								}`}
+							</option>
+						))}
+					</Select>
+				</Box>
+			</Stack>
+		</Flex>
+	);
 };
 
 export default Navigation;
